2f5c66e5f2076bbb711a80eea39d2f5662f66f19
luolonghao <luolonghao@05456332-8429-0410-9396-4db448dfe136>
2010-07-19 04:30:06 +0000

    [branches/4.0] dev

---
+++ branches/4.0/src/config.js
@@ -0,0 +1,90 @@
+/**
+ * KindEditor - WYSIWYG HTML Editor
+ *
+ * Copyright (c) 2010 kindsoft.net All rights reserved.
+ * Released under LGPL License.
+ */
+
+/**
+ * @name config.js
+ * @fileOverview 配置文件
+ * @author Longhao Luo
+ */
+
+/**
+#using "core.js"
+*/
+
+var _LANG_TYPE = 'zh_CN';
+
+var _options = {
+	designMode : true,
+	fullscreenMode : false,
+	filterMode : false,
+	shadowMode : true,
+	scriptPath : _getScriptPath(),
+	urlType : '', //"",relative,absolute,domain
+	newlineType : 'p', //p,br
+	resizeType : 2, //0,1,2
+	dialogAlignType : 'page', //page,editor
+	bodyClass : 'ke-content',
+	cssData : '', //cssPath, cssContent
+	minWidth : 200,
+	minHeight : 100,
+	minChangeSize : 5,
+	items : [
+		'source', '|', 'fullscreen', 'undo', 'redo', 'print', 'cut', 'copy', 'paste',
+		'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
+		'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
+		'superscript', '|', 'selectall', '/',
+		'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
+		'italic', 'underline', 'strikethrough', 'removeformat', '|', 'image',
+		'flash', 'media', 'table', 'hr', 'emoticons', 'link', 'unlink', '|', 'about'
+	],
+	colors : [
+		['#E53333', '#E56600', '#FF9900', '#64451D', '#DFC5A4', '#FFE500'],
+		['#009900', '#006600', '#99BB00', '#B8D100', '#60D978', '#00D5FF'],
+		['#337FE5', '#003399', '#4C33E5', '#9933E5', '#CC33E5', '#EE33EE'],
+		['#FFFFFF', '#CCCCCC', '#999999', '#666666', '#333333', '#000000']
+	],
+	htmlTags : {
+		font : ['color', 'size', 'face', '.background-color'],
+		span : [
+			'.color', '.background-color', '.font-size', '.font-family', '.background',
+			'.font-weight', '.font-style', '.text-decoration', '.vertical-align'
+		],
+		div : [
+			'align', '.border', '.margin', '.padding', '.text-align', '.color',
+			'.background-color', '.font-size', '.font-family', '.font-weight', '.background',
+			'.font-style', '.text-decoration', '.vertical-align', '.margin-left'
+		],
+		table: [
+			'border', 'cellspacing', 'cellpadding', 'width', 'height', 'align', 'bordercolor',
+			'.padding', '.margin', '.border', 'bgcolor', '.text-align', '.color', '.background-color',
+			'.font-size', '.font-family', '.font-weight', '.font-style', '.text-decoration', '.background',
+			'.width', '.height'
+		],
+		'td,th': [
+			'align', 'valign', 'width', 'height', 'colspan', 'rowspan', 'bgcolor',
+			'.text-align', '.color', '.background-color', '.font-size', '.font-family', '.font-weight',
+			'.font-style', '.text-decoration', '.vertical-align', '.background'
+		],
+		a : ['href', 'target', 'name'],
+		embed : ['src', 'width', 'height', 'type', 'loop', 'autostart', 'quality', '.width', '.height', 'align', 'allowscriptaccess', '/'],
+		img : ['src', 'width', 'height', 'border', 'alt', 'title', '.width', '.height', '/'],
+		hr : ['/'],
+		br : ['/'],
+		'p,ol,ul,li,blockquote,h1,h2,h3,h4,h5,h6' : [
+			'align', '.text-align', '.color', '.background-color', '.font-size', '.font-family', '.background',
+			'.font-weight', '.font-style', '.text-decoration', '.vertical-align', '.text-indent', '.margin-left'
+		],
+		'tbody,tr,strong,b,sub,sup,em,i,u,strike' : []
+	}
+};
+
+var _INLINE_TAG_MAP = _toMap('a,abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var'),
+	_BLOCK_TAG_MAP = _toMap('address,applet,blockquote,button,center,dd,del,dir,div,dl,dt,fieldset,form,frameset,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,p,pre,script,table,tbody,td,tfoot,th,thead,tr,ul'),
+	_SINGLE_TAG_MAP = _toMap('area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed'),
+	_AUTOCLOSE_TAG_MAP = _toMap('colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr'),
+	_FILL_ATTR_MAP = _toMap('checked,compact,declare,defer,disabled,ismap,multiple,nohref,noresize,noshade,nowrap,readonly,selected'),
+	_VALUE_TAG_MAP = _toMap('input,button,textarea,select');
--- branches/4.0/src/main.js
+++ branches/4.0/src/main.js
@@ -13,6 +13,7 @@
 
 /**
 #using "core.js"
+#using "config.js"
 #using "node.js"
 #using "main.js"
 */
@@ -36,82 +37,6 @@ function _lang(key, langType) {
 function _pluginLang(key, langType) {
 	return _lang('plugins', langType)[key];
 }
-
-var _scriptPath = (function() {
-	var els = document.getElementsByTagName('script'), src;
-	for (var i = 0, len = els.length; i < len; i++) {
-		src = els[i].src || '';
-		if (src.match(/kindeditor[\w\-\.]*\.js/)) {
-			return src.substring(0, src.lastIndexOf('/') + 1);
-		}
-	}
-	return '';
-})();
-
-var _options = {
-	designMode : true,
-	fullscreenMode : false,
-	filterMode : false,
-	shadowMode : true,
-	scriptPath : _scriptPath,
-	urlType : '', //"",relative,absolute,domain
-	newlineType : 'p', //p,br
-	resizeType : 2, //0,1,2
-	dialogAlignType : 'page', //page,editor
-	bodyClass : 'ke-content',
-	cssData : '', //cssPath, cssContent
-	minWidth : 200,
-	minHeight : 100,
-	minChangeSize : 5,
-	items : [
-		'source', '|', 'fullscreen', 'undo', 'redo', 'print', 'cut', 'copy', 'paste',
-		'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
-		'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
-		'superscript', '|', 'selectall', '/',
-		'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
-		'italic', 'underline', 'strikethrough', 'removeformat', '|', 'image',
-		'flash', 'media', 'table', 'hr', 'emoticons', 'link', 'unlink', '|', 'about'
-	],
-	colors : [
-		['#E53333', '#E56600', '#FF9900', '#64451D', '#DFC5A4', '#FFE500'],
-		['#009900', '#006600', '#99BB00', '#B8D100', '#60D978', '#00D5FF'],
-		['#337FE5', '#003399', '#4C33E5', '#9933E5', '#CC33E5', '#EE33EE'],
-		['#FFFFFF', '#CCCCCC', '#999999', '#666666', '#333333', '#000000']
-	],
-	htmlTags : {
-		font : ['color', 'size', 'face', '.background-color'],
-		span : [
-			'.color', '.background-color', '.font-size', '.font-family', '.background',
-			'.font-weight', '.font-style', '.text-decoration', '.vertical-align'
-		],
-		div : [
-			'align', '.border', '.margin', '.padding', '.text-align', '.color',
-			'.background-color', '.font-size', '.font-family', '.font-weight', '.background',
-			'.font-style', '.text-decoration', '.vertical-align', '.margin-left'
-		],
-		table: [
-			'border', 'cellspacing', 'cellpadding', 'width', 'height', 'align', 'bordercolor',
-			'.padding', '.margin', '.border', 'bgcolor', '.text-align', '.color', '.background-color',
-			'.font-size', '.font-family', '.font-weight', '.font-style', '.text-decoration', '.background',
-			'.width', '.height'
-		],
-		'td,th': [
-			'align', 'valign', 'width', 'height', 'colspan', 'rowspan', 'bgcolor',
-			'.text-align', '.color', '.background-color', '.font-size', '.font-family', '.font-weight',
-			'.font-style', '.text-decoration', '.vertical-align', '.background'
-		],
-		a : ['href', 'target', 'name'],
-		embed : ['src', 'width', 'height', 'type', 'loop', 'autostart', 'quality', '.width', '.height', 'align', 'allowscriptaccess', '/'],
-		img : ['src', 'width', 'height', 'border', 'alt', 'title', '.width', '.height', '/'],
-		hr : ['/'],
-		br : ['/'],
-		'p,ol,ul,li,blockquote,h1,h2,h3,h4,h5,h6' : [
-			'align', '.text-align', '.color', '.background-color', '.font-size', '.font-family', '.background',
-			'.font-weight', '.font-style', '.text-decoration', '.vertical-align', '.text-indent', '.margin-left'
-		],
-		'tbody,tr,strong,b,sub,sup,em,i,u,strike' : []
-	}
-};
 
 function KEditor(options) {
 	var self = this;
